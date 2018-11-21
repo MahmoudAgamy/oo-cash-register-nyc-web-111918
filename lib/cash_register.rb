7e65ea5c261571acf0be4edb40cb3b9a1d548b7e
Mahmoud Agamy <agamyv@gmail.com>
2018-11-21 23:05:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,12 +17,15 @@ class CashRegister
     end
     
     def apply_discount
+      if @discount == 0 
+        return "There is no discount to apply." if @discount == 0
+      else
+        discount_val = (self.total * (self.discount/100))
+        total_val = self.total
+        @total = total_val - discount_val
+        #puts "After the discount, the total comes to #{@total}"
+      end
       
-      discount_val = (self.total * (self.discount/100))
-      total_val = self.total
-      @total = total_val - discount_val
-      #puts "After the discount, the total comes to #{@total}"
-      return "There is no discount to apply." if @discount == 0
     end
     
     def items
